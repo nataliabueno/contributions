8bb3061d7c0cc8d32f8162c528ccbe89f1d0f7bd
Natalia Bueno <natalia.bueno@yale.edu>
2017-08-05 20:02:33 -0300

    second attempt

--- conversao.R
+++ conversao.R
@@ -5,6 +5,17 @@
 rm(list=ls())
 
 
+doInstall <- TRUE  # Change to FALSE if you don't want packages installed.
+toInstall <- c("ROAuth", "streamR", "ggplot2", "stringr",
+               "maps", "Rfacebook", "devtools", "quanteda",
+               "igraph", "rvest", "httr", "yaml", "reshape", "formatR", "readtext",
+               "stringi", "corpus", "Unicode", "ghit", 
+               "gtools", "tm", "readxl", "xlsx", "tesseract")
+if (doInstall) install.packages(toInstall)
+if (doInstall) devtools::install_github("netdem-usc/netdemR")
+#ghit::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"))
+
+
 library("openxlsx")
 library(stringr)
 library(stringi)
@@ -12,18 +23,26 @@ library(gtools)
 library(tm)
 library(readxl)
 library(xlsx)
+library(tesseract)
+
 options(scipen = 999) #disabling sci notation
 
 
-home <- "~/Dropbox/"
+home <- "~/Desktop/temp/Contributions/00002201/"
+
+system("pdftotext")
+system("pdftotext ~/Desktop/temp/Contributions/00002201/p13.pdf")
 
 #These components need to be installed...
 file.exists(Sys.which(c("pdfinfo", "pdftotext")))
 file.exists(Sys.which(c("pdf_info", "ps2ascii")))
 
 tt <- readPDF(control = list(text = "-layout"))
-rr <- tt(elem = list(uri = "file_to_beconverted.pdf"), language = "pt", id = "id1")
+rr <- tt(elem = list(uri = "~/Desktop/temp/Contributions/00002201/p14.pdf"), language = "pt", id = "id1")
 txt <- iconv(rr$content, from = "latin1")
 
-save(txt, file = "file.RData")
+
+
+text <- ocr(paste0(home, "p13.pdf"))
+cat(text)
 
